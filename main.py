2423f90e182ae1f0a9b512d0df5adf87610e9797
HachiMinh <chichicvc1234@gmail.com>
2021-09-06 22:51:51 +0700

    i

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 
 TOKEN = os.environ["TOKEN"]
-bot = commands.Bot(command_prefix = "[")
+bot = commands.Bot(command_prefix = "[",intents=discord.Intents.all())
 logging.basicConfig(level = logging.INFO)
 
 @bot.event
